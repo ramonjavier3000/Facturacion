fb2b00514dcc5a31c318abbe3591ee663362cadc
Ramonjavier3000 <2020-0553@uteco.edu.do>
2023-01-27 18:12:58 -0400

    proyecto

--- Server/Models/Usuario.cs
+++ Server/Models/Usuario.cs
@@ -1,26 +1,24 @@
-using System.ComponentModel.DataAnnotatios;
 using facturacion_2023.Shared.Requests;
 
 namespace facturacion_2023.Server.Models;
 
 //Representa la esctructura de datos del usuario.(la tabla en la DB).
 public class Usuario
-
 {
- [Key]
+ [System.ComponentModel.DataAnnotations.Key]
  public int Id {get;set;}
  public int usuarioRolId { get; set;}
  public UsuarioRol UsuarioRol {get;set;} = default!;
  public string Name {get; set;} = null!;
  public string Nickname {get; set;} = null!;
  public string Password {get; set;} = null!;
- public virtual Icollection<UsuarioRol>? ususario {get; set;}
+ public virtual ICollection<UsuarioRol>? ususario {get; set;}
 
- public static Ususario Crear(UsuarioRequest request)
+ public static Usuario Crear(UsuarioRequest request)
  {
-    return new Ususario(){
-        UsuarioRolId = request.UsuarioRolId,
-        Name.request.Name,
+    return new Usuario(){
+        usuarioRolId = request.UsuarioRolId,
+        Name = request.Name,
         Nickname = request.Nickname,
         Password = request.Password
 
--- Server/Models/UsuarioRol.cs
+++ Server/Models/UsuarioRol.cs
@@ -2,14 +2,14 @@ namespace facturacion_2023.Server.Models;
 
     public class UsuarioRol
 {
-    [Key]
+    [System.ComponentModel.DataAnnotations.Key]
  public int Id {get;set;}
  public string Name {get; set;} = null!;
  public  int MyProperty {get; set;}
  public bool permisoParaCrear {get; set;}
  public bool permisoParaEditar {get; set;}
  public bool permisoParaEliminar {get; set;}
-    public virtual  IColletion<UsuarioRol>? Usuarios {get; set; }
+    public virtual  ICollection<UsuarioRol>? Usuarios {get; set; }
 
 
 }
